ab4717b7af20c7af5a40e927817a3abee3239f4b
daniawati <daniawati870@gmail.com>
2019-10-25 21:31:29 +0700

    upload kuis 01

---
+++ 2120180142-K01-PBO-TI20192020-3-4-02/src/pkg2120180142/k01/pbo/ti20192020/pkg3/pkg4/pkg02/K01PBOTI201920203402.java
@@ -0,0 +1,27 @@
+package pkg2120180142.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class K01PBOTI201920203402 {
+    public static void main(String[] args) {
+        //instansiasi dari kelas Mhs
+        //membuat objek teman1 dari kelas Mhs
+        Mhs teman=new Mhs();
+        //membuat objek teman1 dari kelas Mhs
+        Mhs teman1=new Mhs();
+        //membuat objek teman2 dari kelas Mhs
+        Mhs teman2=new Mhs();
+        //membuat objek teman3 dari kelas Mhs
+        Mhs teman3=new Mhs();
+        //membuat objek teman4 dari kelas Mhs
+        Mhs teman4=new Mhs();
+        
+        System.out.println("Biodata Mhs     :\n");//memunculkan tulisan "Biodata Mhs     :"
+        //diatas hasil dari akses method
+        
+    //akses method
+        //memasukan biodata 5 teman dan memanggil method
+        teman.tampilBiodata(" Lia Agustina"," 2120180165"," Bojonegoro");
+        teman1.tampilBiodata(" Dedy Gilang R. M."," 2120180143"," Bojonegoro");
+        teman2.tampilBiodata(" Siti Gurotul B."," 2120180201"," Bojonegoro");
+        teman3.tampilBiodata(" Moh. Hanif I."," 2120180179"," Bojonegoro");
+        teman4.tampilBiodata(" Nuruz Zilvia"," 2120180196"," Bojonegoro");
+    }
+}
